0fbcd00660c62aee2333d56f30db3b806118bff0
bivilfd <bivil@freshdesk.com>
2015-08-07 16:40:24 +0530

    Class file

---
+++ Class.rb
@@ -0,0 +1,26 @@
+ class Computer
+    @@users = {}
+    def initialize(username, password)
+        @username = username
+        @password = password
+        @files = {}
+        @@users[username] = password
+    end
+    
+    def create(filename)
+        time = Time.now
+        @files[filename] = time
+        puts "A new file named #{filename} was created at #{@files[filename]} by #{@username}"
+    end
+    
+    def Computer.get_users
+        @@users
+    end
+end
+
+my_computer = Computer.new("bivil","****")
+my_computer.create("text")
+your = Computer.new("ben","***")
+your.create("fh")
+
+puts Computer.get_users
